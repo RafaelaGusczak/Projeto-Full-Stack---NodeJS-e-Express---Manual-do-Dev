a7a54aef6372c6ff319a07228cf1e1109e0eb116
RafaelaGusczak <rafaela.gusczak@gmail.com>
2025-02-07 11:46:49 -0300

    integração finalizada

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,9 +1,56 @@
 const tbody = document.querySelector('tbody')
+const addForm = document.querySelector('.add-form');
+const inputTask = document.querySelector('.input-task');
 
 const fetchTasks = async () => {
     const response = await fetch('http://localhost:3333/tasks');
     const tasks = await response.json()
     return tasks;
+}
+
+const addTask = async (event) => {
+    event.preventDefault();
+
+    const task = { title: inputTask.value }
+
+    await fetch('http://localhost:3333/tasks', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(task),
+    });
+
+    loadTasks();
+    inputTask.value = '';
+}
+
+const deleteTask = async (id) => {
+    await fetch(`http://localhost:3333/tasks/${id}`, {
+        method: 'delete',
+    });
+
+    loadTasks();
+}
+
+const updateTask = async ({ id, title, status }) => {
+
+    await fetch(`http://localhost:3333/tasks/${id}`, {
+        method: 'put',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({title, status}),
+    });
+
+    loadTasks();
+
+}
+
+const formatDate = (dateUTC) => {
+    const options = { dateStyle: 'long', timeStyle: 'short'};
+    const date = new Date(dateUTC).toLocaleString('pt-br', options);
+    return date;
 }
 
 const createElement = (tag, innerText = '', innerHTML = '') => {
@@ -21,11 +68,19 @@ const createElement = (tag, innerText = '', innerHTML = '') => {
     return element;
 }
 
-const task = {
-    id: 1,
-    title: 'oi',
-    created_at: 'data',
-    status: 'pendente'
+const createSelect = (value) => {
+
+    const options = `
+        <option value="pendente">pendente</option>
+        <option value="em andamento">em andamento</option>
+        <option value="concluída">concluída</option>
+    `;
+
+    const select = createElement('select', '', options);
+
+    select.value = value;
+
+    return select;
 }
 
 const createRow = (task) => {
@@ -34,15 +89,63 @@ const createRow = (task) => {
 
     const tr = createElement('tr');
     const tdTitle = createElement('td', title);
-    const tdCreatedAt = createElement('td', created_at);
+    const tdCreatedAt = createElement('td', formatDate(created_at));
     const tdStatus = createElement('td');
     const tdActions = createElement('td');
 
+    const select = createSelect(status);
+
+    select.addEventListener('change', ({ target }) => updateTask({ ...task, status: target.value }));
+
     const editButton = createElement('button', '', '<span class="material-symbols-outlined">edit</span>');
+    const deleteButton = createElement('button', '', '<span class="material-symbols-outlined">delete</span>');
    
-    tr.appendChild(editButton);
-    tbody.appendChild(tr)
+    const editForm = createElement('form');
+    const editInput = createElement('input');
+
+    editInput.value = title;
+    editForm.appendChild(editInput);
+
+    editForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        
+        updateTask({ id, title: editInput.value, status });
+    });
+
+    editButton.addEventListener('click', () => {
+        tdTitle.innerText = '';
+        tdTitle.appendChild(editForm);
+    });
+
+    editButton.classList.add('btn-action');
+    deleteButton.classList.add('btn-action');
+
+    deleteButton.addEventListener('click', () => {deleteTask(id)});
+
+    tdStatus.appendChild(select);
+
+    tdActions.appendChild(editButton);
+    tdActions.appendChild(deleteButton);
+
+    tr.appendChild(tdTitle);
+    tr.appendChild(tdCreatedAt);
+    tr.appendChild(tdStatus);
+    tr.appendChild(tdActions);
+
+    return tr;
 }
 
-createRow(task)
+const loadTasks =  async () => {
+    const tasks = await fetchTasks();
 
+    tbody.innerHTML = '';
+
+    tasks.forEach((task) => {
+        const tr = createRow(task);
+        tbody.appendChild(tr);
+    });
+}
+
+addForm.addEventListener('submit', addTask);
+
+loadTasks();
